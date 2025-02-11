908253892eed3a2d0e84494a18dbb4692779876d
Nitish <nitish@Nitishs-MacBook-Air.local>
2025-02-11 14:11:30 +0530

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("PORT:", process.env.PORT);
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
+console.log("ADMIN_API_KEY:", process.env.ADMIN_API_KEY)
+
+
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Test the connection
+pool.getConnection()
+    .then(connection => {
+        console.log('Database connected successfully');
+        connection.release();
+    })
+    .catch(err => {
+        console.error('Error connecting to the database:', err);
+    });
+
+module.exports = pool;
